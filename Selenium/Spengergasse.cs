4e2b89b65f1e908b129446a74154d07b0602eb38
Dawud Hussein <dawud.hussein@outlook.com>
2022-01-10 12:43:17 +0100

    Selenium Test initial commit

---
+++ Selenium/Spengergasse.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Opera;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Selenium
+{
+    class Spengergasse
+    {
+        static void Main(string[] args)
+        {
+            //NugetPackages Required
+            new DriverManager().SetUpDriver(new OperaConfig());
+
+            IWebDriver driver = new OperaDriver();
+
+            driver.Url = "https://www.duckduckgo.com";
+
+            IWebElement element = driver.FindElement(By.XPath("//*[@id='search_form_input_homepage']"));
+
+            element.SendKeys("LambdaTest");
+
+            /* Submit the Search */
+            element.Submit();
+
+            /* Perform wait to check the output */
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
