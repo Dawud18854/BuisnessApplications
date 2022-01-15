204491a29a19528befeae86a579c6a0aeb68aa6c
Dawud Hussein <dawud.hussein@outlook.com>
2022-01-15 12:28:42 +0100

    Added Simple Property Tests for Addition

--- PropertyBasedTesting.Test/xUnitPBT.cs
+++ PropertyBasedTesting.Test/xUnitPBT.cs
@@ -1,13 +1,35 @@
+using FsCheck;
+using FsCheck.Xunit;
 using Xunit;
 
 namespace PropertyBasedTesting.Test
 {
     public class xUnitPBT
     {
-        [Fact]
-        public void Test1()
-        {
+        //PropertyTesting with xUnit using FsCheck
 
+
+        [Property]
+        public Property Test_x_Times_2_Equals_x_Plus_x(int x)
+        {
+            return (x * 2 == Add(x, x)).ToProperty();
+        }
+
+        [Property]
+        public Property Test_x_Plus_1_Plus_1_Equals_x_Plus_2(int x)
+        {
+            return (Add(1, Add(1, x)) == Add(x, 2)).ToProperty();
+        }
+
+        [Property]
+        public Property Test_Adding_Order_Doenst_Matter(int x, int y)
+        {
+            return (Add(x, y) == Add(y, x)).ToProperty();
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
